2861574c2e5d5f887fe1fdd4d57656be6b2bbdbf
Ricardo Alves - Ext <ricardo.alves.ext@brf.com>
2023-05-18 17:53:03 -0300

    Add project files.

---
+++ src/SampleRedPanda.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace SampleRedPanda.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9015",
+            ClientId = "redpanda-producer",
+            Acks = Acks.All
+        };
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            var topic = "msg-produce"; // Substitua pelo nome do tópico desejado
+
+            for (var i = 0; i < 10; i++)
+            {
+                var message = $"Mensagem {i}";
+
+                var deliveryResult = producer.ProduceAsync(topic, new Message<Null, string> { Value = message }).GetAwaiter().GetResult();
+                System.Console.WriteLine($"Mensagem enviada: {message} | Offset: {deliveryResult.Offset}");
+            }
+
+            producer.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
